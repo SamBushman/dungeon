98a2712717ed84f80e0c08f502fb24547158c7d9
Sam Bushman <sbushman@gmail.com>
2026-09-13 00:49:22 -0700

    Initial commit of project files

---
+++ CameraPivot.gd
@@ -0,0 +1,25 @@
+extends Position3D
+
+export var speed = 14
+
+var velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$".".look_at(translation + direction, Vector3.UP)
+		
+	velocity.x = direction.x + speed
+	velocity.z = direction.z + speed
+	velocity = $Camera.move_and_slide(velocity, Vector3.UP)
